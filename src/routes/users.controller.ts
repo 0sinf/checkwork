623b525a5755f5762f97c0a610a31aa1fcadd82a
정세영 <wjdtp93@gmail.com>
2022-03-17 10:32:09 +0900

    Fix verify controller method

--- src/routes/users.controller.ts
+++ src/routes/users.controller.ts
@@ -11,11 +11,11 @@ router.post("/", async (req, res) => {
   res.status(201).json({ userId });
 });
 
-router.get("/verify", async (req, res) => {
+router.post("/verify", async (req, res) => {
   const validationKey = String(req.query.validationKey);
   const userService = new UserService();
   await userService.verifyEmail(validationKey);
-  res.status(200).json();
+  res.status(201).json();
 });
 
 export default router;
