a2cafba2f9e37aab7383517587973502f59c7773
정세영 <wjdtp93@gmail.com>
2022-03-18 07:56:22 +0900

    Add user login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import user from "./users.controller";
+import auth from "./auths.controller";
 
 const router = Router();
 
+router.use("/auth", auth);
 router.use("/users", user);
 
 export default router;
