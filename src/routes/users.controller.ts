012442441b067a8156abf62eab5fd184e3c3290f
정세영 <wjdtp93@gmail.com>
2022-03-20 20:44:49 +0900

    Add async-handler in router

--- src/routes/users.controller.ts
+++ src/routes/users.controller.ts
@@ -3,43 +3,62 @@ import { Router } from "express";
 import { UserService } from "../services/users.service";
 import loginRequired from "../middlewares/loginRequired";
 import checkUser from "../utils/checkUser";
+import asyncHandler from "../utils/asyncHandler";
 
 const router = Router();
 
-router.get("/:id", loginRequired, async (req, res) => {
-  const userId = Number(req.params.id);
-  const token = req.headers.authorization.split("Bearer ")[1];
-  const { id } = checkUser(token, userId);
+router.get(
+  "/:id",
+  loginRequired,
+  asyncHandler(async (req, res) => {
+    const userId = Number(req.params.id);
+    const token = req.headers.authorization.split("Bearer ")[1];
+    const { id } = checkUser(token, userId);
 
-  const userService = new UserService();
-  const user = await userService.getUserInfo(id);
-  res.json({ user });
-});
+    const userService = new UserService();
+    const user = await userService.getUserInfo(id);
+    res.json({ user });
+  })
+);
 
-router.post("/", async (req, res) => {
-  const { email, password, passwordConfirm } = req.body;
-  const userService = new UserService();
-  const userId = await userService.createUser(email, password, passwordConfirm);
-  res.status(201).json({ userId });
-});
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { email, password, passwordConfirm } = req.body;
+    const userService = new UserService();
+    const userId = await userService.createUser(
+      email,
+      password,
+      passwordConfirm
+    );
+    res.status(201).json({ userId });
+  })
+);
 
-router.post("/verify", async (req, res) => {
-  const validationKey = String(req.query.validationKey);
-  const userService = new UserService();
-  const token = await userService.verifyEmail(validationKey);
-  res.status(201).json({ token });
-});
+router.post(
+  "/verify",
+  asyncHandler(async (req, res) => {
+    const validationKey = String(req.query.validationKey);
+    const userService = new UserService();
+    const token = await userService.verifyEmail(validationKey);
+    res.status(201).json({ token });
+  })
+);
 
-router.patch("/:id", loginRequired, async (req, res) => {
-  const userId = Number(req.params.id);
-  const token = req.headers.authorization.split("Bearer ")[1];
-  const { id } = checkUser(token, userId);
+router.patch(
+  "/:id",
+  loginRequired,
+  asyncHandler(async (req, res) => {
+    const userId = Number(req.params.id);
+    const token = req.headers.authorization.split("Bearer ")[1];
+    const { id } = checkUser(token, userId);
 
-  const { wage } = req.body;
-  const userService = new UserService();
-  await userService.updateWage(id, Number(wage));
+    const { wage } = req.body;
+    const userService = new UserService();
+    await userService.updateWage(id, Number(wage));
 
-  res.json();
-});
+    res.json();
+  })
+);
 
 export default router;
