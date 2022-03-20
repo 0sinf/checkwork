2dd83be73d362ca2cce398d6390c4fea31b9428e
Young <43630508+young-sinf@users.noreply.github.com>
2022-03-20 20:25:46 +0900

    Add error handler (#16)

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { createConnection } from "typeorm";
 
 import config from "./config";
@@ -7,10 +7,6 @@ import route from "./routes";
 const app = express();
 const port = config.port || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/api", route);
-
 createConnection()
   .then(() => {
     console.log("Database connected");
@@ -18,6 +14,19 @@ createConnection()
   .catch((error) => {
     console.log(error);
   });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", route);
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ msg: "Not Found" });
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(403).json({ msg: err.message });
+  next();
+});
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(port, () => {
