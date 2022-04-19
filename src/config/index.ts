78b4acc52daedeff4f03829a8bc18c42475f32e1
정세영 <wjdtp93@gmail.com>
2022-04-19 13:51:49 +0900

    Add db pool

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,10 @@
 export default {
   port: Number(process.env.PORT) || 3000,
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: Number(process.env.DB_PORT) || 5432,
+  },
 };
