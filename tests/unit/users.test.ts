130af6b027819d1fafb4161f5e71152f8c6613a0
정세영 <wjdtp93@gmail.com>
2022-04-20 21:02:48 +0900

    Add update user

--- tests/unit/users.test.ts
+++ tests/unit/users.test.ts
@@ -14,6 +14,7 @@ let req: MockRequest<Request>, res: MockResponse<Response>, next: NextFunction;
 jest.mock("../../src/models/users.model", () => ({
   save: jest.fn(),
   findById: jest.fn(),
+  findByIdAndUpdate: jest.fn(),
 }));
 
 const newUser = {
@@ -28,6 +29,48 @@ beforeEach(() => {
   req = createRequest();
   res = createResponse();
   next = jest.fn();
+});
+
+describe("user controller updateUser", () => {
+  const updateUser = {
+    company: "updated company",
+    wage: 15000,
+  };
+
+  beforeEach(() => {
+    req.body = updateUser;
+    req.params.userId = userId;
+  });
+
+  it("should exist updateUser", () => {
+    expect(typeof userController.updateUser).toEqual("function");
+  });
+
+  it("should call findByIdAndUpdate", async () => {
+    await userController.updateUser(req, res, null);
+    expect(userRepository.findByIdAndUpdate).toBeCalledWith(
+      +userId,
+      updateUser
+    );
+  });
+
+  it("should return 200 and json", async () => {
+    (userRepository.findByIdAndUpdate as jest.Mock).mockReturnValue(updateUser);
+    await userController.updateUser(req, res, null);
+    expect(res.statusCode).toEqual(200);
+    expect(res._getJSONData()).toEqual(updateUser);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should handle errors", async () => {
+    const error = { message: "error" };
+    const rejectedPromise = Promise.reject(error);
+    (userRepository.findByIdAndUpdate as jest.Mock).mockReturnValue(
+      rejectedPromise
+    );
+    await userController.updateUser(req, res, next);
+    expect(next).toBeCalledWith(error);
+  });
 });
 
 describe("user controller getUser", () => {
