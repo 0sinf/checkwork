9001726ab16e903023f71c32a8c9ddc043759c57
정세영 <wjdtp93@gmail.com>
2022-04-20 16:53:46 +0900

    Add user route

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 
 import config from "./config";
+import userRouter from "./routes/users";
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/users", userRouter);
 
 app.listen(config.port, () => {
   console.log("Start App");
