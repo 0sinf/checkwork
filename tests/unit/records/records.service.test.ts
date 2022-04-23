8ed8323e90535537c8f27f236758fbc64589690a
정세영 <wjdtp93@gmail.com>
2022-04-23 09:43:19 +0900

    Add create record in service

---
+++ tests/unit/records/records.service.test.ts
@@ -0,0 +1,26 @@
+import recordRepository from "../../../src/models/records.model";
+import recordService from "../../../src/services/records.service";
+
+jest.mock("../../../src/models/records.model", () => ({
+  save: jest.fn(),
+}));
+
+const now = new Date();
+
+const record = {
+  day: now,
+  startTime: now,
+  endTime: now,
+};
+
+describe("record service save", () => {
+  it("should exist createRecord", () => {
+    expect(typeof recordService.createRecord).toEqual("function");
+  });
+
+  it("should call save", async () => {
+    await recordService.createRecord(record);
+    console.log(record);
+    expect(recordRepository.save).toBeCalledWith(record);
+  });
+});
